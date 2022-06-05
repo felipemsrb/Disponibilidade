9b28b97e49353d7478d1a280ae69488bac8e05b2
Felipe Mota <felipe.msrb@gmail.com>
2022-06-05 10:47:11 +0100

    adicionando arquivo jss e finalizando projeto

--- main.js
+++ main.js
@@ -1,8 +1,34 @@
-var single= [1, 2, 3];
-var twins= [1, 2, 3];
-var triplo= [1, 2, 3];
+let quartoSingle= 3;
+let quartoTwin= 3;
+let quartoTriplo= 3;
 
 
-function reserva (texto) {
-    alert (texto)
+
+function reserva () {
+    let formulario = document.getElementById("formulario");
+   let quartoSelecionado = formulario.tipoquarto.value;
+   let numeroDePessoas = formulario.pessoas.value;
+   
+   
+
+   switch (quartoSelecionado) {
+       case 'single': 
+        quartoSingle--;
+        break;
+
+       case 'twin': 
+        quartoTwin--;
+        break;
+
+       case 'triplo': 
+        quartoTriplo--;
+        break;
+
+        default: alert('Selecione o tipo de quarto');
+        return;
+   }
+
+   alert(`Sua reserva para ${numeroDePessoas} pessoas foi concluída. Agora temos ${quartoSingle} quartos singles, ${quartoTwin} quartos twins e ${quartoTriplo} quartos triplos`);
 }
+
+
